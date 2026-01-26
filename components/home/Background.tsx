7c5309f770ede0854763290ae740dfc96eecb1d7
Kateryna Skoryna <k.skoryna@gmail.com>
2026-01-26 16:33:08 +0100

    update background div layout

--- components/home/Background.tsx
+++ components/home/Background.tsx
@@ -9,9 +9,11 @@ export default function Background() {
   return (
     <div
       className="
-        fixed inset-0 -z-20 invert-bg-color
-        [clip-path:polygon(70%_0,_100%_0,_100%_100%,_70%_100%)]
-      "
+    fixed inset-0 -z-20 bg-[var(--invert-bg-color)]
+    [clip-path:polygon(100%_0,_100%_0,_100%_100%,_100%_100%)]
+    md:[clip-path:polygon(70%_0,_100%_0,_100%_100%,_70%_100%)]
+    lg:[clip-path:polygon(90%_0,_100%_0,_100%_100%,_90%_100%)]
+  "
     />
   );
 }
